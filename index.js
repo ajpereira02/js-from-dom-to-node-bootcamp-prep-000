f1fb0c356612420ad3a1c83aca059acfa9ad853f
Ashley Pereira <ashleypereira70@gmail.com>
2018-06-24 20:57:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 // write your code below!
 console.log("Hello World!")
-nodejsindex.js.nodejs
-cdEXACT-FOLDER-YOU-WANT-TO-ENTER
-nodejsindex.js.nodejsindex
-console.log("Hello World!")
-cdEXACT-FOLDER-YOU-WANT-TO-ENTER
-
